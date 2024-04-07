297d89493997d9f86b0bd6a0de3773ea58f53d32
ampcpmgp <ampcpmgp@gmail.com>
2024-04-07 22:26:43 +0900

    add category

--- packages/element/src/types/elements.ts
+++ packages/element/src/types/elements.ts
@@ -3,589 +3,707 @@ import type { Element } from "..";
 export interface Hydrogen extends Element {
   symbol: "H";
   number: 1;
+  category: "Nonmetal";
 }
 
 export interface Helium extends Element {
   symbol: "He";
   number: 2;
+  category: "Noble Gas";
 }
 
 export interface Lithium extends Element {
   symbol: "Li";
   number: 3;
+  category: "Alkali Metal";
 }
 
 export interface Beryllium extends Element {
   symbol: "Be";
   number: 4;
+  category: "Alkaline Earth Metal";
 }
 
 export interface Boron extends Element {
   symbol: "B";
   number: 5;
+  category: "Metalloid";
 }
 
 export interface Carbon extends Element {
   symbol: "C";
   number: 6;
+  category: "Nonmetal";
 }
 
 export interface Nitrogen extends Element {
   symbol: "N";
   number: 7;
+  category: "Nonmetal";
 }
 
 export interface Oxygen extends Element {
   symbol: "O";
   number: 8;
+  category: "Nonmetal";
 }
 
 export interface Fluorine extends Element {
   symbol: "F";
   number: 9;
+  category: "Halogen";
 }
 
 export interface Neon extends Element {
   symbol: "Ne";
   number: 10;
+  category: "Noble Gas";
 }
 
 export interface Sodium extends Element {
   symbol: "Na";
   number: 11;
+  category: "Alkali Metal";
 }
 
 export interface Magnesium extends Element {
   symbol: "Mg";
   number: 12;
+  category: "Alkaline Earth Metal";
 }
 
 export interface Aluminum extends Element {
   symbol: "Al";
   number: 13;
+  category: "Post-transition Metal";
 }
 
 export interface Silicon extends Element {
   symbol: "Si";
   number: 14;
+  category: "Metalloid";
 }
 
 export interface Phosphorus extends Element {
   symbol: "P";
   number: 15;
+  category: "Nonmetal";
 }
 
 export interface Sulfur extends Element {
   symbol: "S";
   number: 16;
+  category: "Nonmetal";
 }
 
 export interface Chlorine extends Element {
   symbol: "Cl";
   number: 17;
+  category: "Halogen";
 }
 
 export interface Argon extends Element {
   symbol: "Ar";
   number: 18;
+  category: "Noble Gas";
 }
 
 export interface Potassium extends Element {
   symbol: "K";
   number: 19;
+  category: "Alkali Metal";
 }
 
 export interface Calcium extends Element {
   symbol: "Ca";
   number: 20;
+  category: "Alkaline Earth Metal";
 }
 
 export interface Scandium extends Element {
   symbol: "Sc";
   number: 21;
+  category: "Transition Metal";
 }
 
 export interface Titanium extends Element {
   symbol: "Ti";
   number: 22;
+  category: "Transition Metal";
 }
 
 export interface Vanadium extends Element {
   symbol: "V";
   number: 23;
+  category: "Transition Metal";
 }
 
 export interface Chromium extends Element {
   symbol: "Cr";
   number: 24;
+  category: "Transition Metal";
 }
 
 export interface Manganese extends Element {
   symbol: "Mn";
   number: 25;
+  category: "Transition Metal";
 }
 
 export interface Iron extends Element {
   symbol: "Fe";
   number: 26;
+  category: "Transition Metal";
 }
 
 export interface Cobalt extends Element {
   symbol: "Co";
   number: 27;
+  category: "Transition Metal";
 }
 
 export interface Nickel extends Element {
   symbol: "Ni";
   number: 28;
+  category: "Transition Metal";
 }
 
 export interface Copper extends Element {
   symbol: "Cu";
   number: 29;
+  category: "Transition Metal";
 }
 
 export interface Zinc extends Element {
   symbol: "Zn";
   number: 30;
+  category: "Transition Metal";
 }
 
 export interface Gallium extends Element {
   symbol: "Ga";
   number: 31;
+  category: "Post-transition Metal";
 }
 
 export interface Germanium extends Element {
   symbol: "Ge";
   number: 32;
+  category: "Metalloid";
 }
 
 export interface Arsenic extends Element {
   symbol: "As";
   number: 33;
+  category: "Metalloid";
 }
 
 export interface Selenium extends Element {
   symbol: "Se";
   number: 34;
+  category: "Nonmetal";
 }
 
 export interface Bromine extends Element {
   symbol: "Br";
   number: 35;
+  category: "Halogen";
 }
 
 export interface Krypton extends Element {
   symbol: "Kr";
   number: 36;
+  category: "Noble Gas";
 }
 
 export interface Rubidium extends Element {
   symbol: "Rb";
   number: 37;
+  category: "Alkali Metal";
 }
 
 export interface Strontium extends Element {
   symbol: "Sr";
   number: 38;
+  category: "Alkaline Earth Metal";
 }
 
 export interface Yttrium extends Element {
   symbol: "Y";
   number: 39;
+  category: "Transition Metal";
 }
 
 export interface Zirconium extends Element {
   symbol: "Zr";
   number: 40;
+  category: "Transition Metal";
 }
 
 export interface Niobium extends Element {
   symbol: "Nb";
   number: 41;
+  category: "Transition Metal";
 }
 
 export interface Molybdenum extends Element {
   symbol: "Mo";
   number: 42;
+  category: "Transition Metal";
 }
 
 export interface Technetium extends Element {
   symbol: "Tc";
   number: 43;
+  category: "Transition Metal";
 }
 
 export interface Ruthenium extends Element {
   symbol: "Ru";
   number: 44;
+  category: "Transition Metal";
 }
 
 export interface Rhodium extends Element {
   symbol: "Rh";
   number: 45;
+  category: "Transition Metal";
 }
 
 export interface Palladium extends Element {
   symbol: "Pd";
   number: 46;
+  category: "Transition Metal";
 }
 
 export interface Silver extends Element {
   symbol: "Ag";
   number: 47;
+  category: "Transition Metal";
 }
 
 export interface Cadmium extends Element {
   symbol: "Cd";
   number: 48;
+  category: "Transition Metal";
 }
 
 export interface Indium extends Element {
   symbol: "In";
   number: 49;
+  category: "Post-transition Metal";
 }
 
 export interface Tin extends Element {
   symbol: "Sn";
   number: 50;
+  category: "Post-transition Metal";
 }
 
 export interface Antimony extends Element {
   symbol: "Sb";
   number: 51;
+  category: "Metalloid";
 }
 
 export interface Tellurium extends Element {
   symbol: "Te";
   number: 52;
+  category: "Metalloid";
 }
 
 export interface Iodine extends Element {
   symbol: "I";
   number: 53;
+  category: "Halogen";
 }
 
 export interface Xenon extends Element {
   symbol: "Xe";
   number: 54;
+  category: "Noble Gas";
 }
 
 export interface Cesium extends Element {
   symbol: "Cs";
   number: 55;
+  category: "Alkali Metal";
 }
 
 export interface Barium extends Element {
   symbol: "Ba";
   number: 56;
+  category: "Alkaline Earth Metal";
 }
 
 export interface Lanthanum extends Element {
   symbol: "La";
   number: 57;
+  category: "Lanthanide";
 }
 
 export interface Cerium extends Element {
   symbol: "Ce";
   number: 58;
+  category: "Lanthanide";
 }
 
 export interface Praseodymium extends Element {
   symbol: "Pr";
   number: 59;
+  category: "Lanthanide";
 }
 
 export interface Neodymium extends Element {
   symbol: "Nd";
   number: 60;
+  category: "Lanthanide";
 }
 
 export interface Promethium extends Element {
   symbol: "Pm";
   number: 61;
+  category: "Lanthanide";
 }
 
 export interface Samarium extends Element {
   symbol: "Sm";
   number: 62;
+  category: "Lanthanide";
 }
 
 export interface Europium extends Element {
   symbol: "Eu";
   number: 63;
+  category: "Lanthanide";
 }
 
 export interface Gadolinium extends Element {
   symbol: "Gd";
   number: 64;
+  category: "Lanthanide";
 }
 
 export interface Terbium extends Element {
   symbol: "Tb";
   number: 65;
+  category: "Lanthanide";
 }
 
 export interface Dysprosium extends Element {
   symbol: "Dy";
   number: 66;
+  category: "Lanthanide";
 }
 
 export interface Holmium extends Element {
   symbol: "Ho";
   number: 67;
+  category: "Lanthanide";
 }
 
 export interface Erbium extends Element {
   symbol: "Er";
   number: 68;
+  category: "Lanthanide";
 }
 
 export interface Thulium extends Element {
   symbol: "Tm";
   number: 69;
+  category: "Lanthanide";
 }
 
 export interface Ytterbium extends Element {
   symbol: "Yb";
   number: 70;
+  category: "Lanthanide";
 }
 
 export interface Lutetium extends Element {
   symbol: "Lu";
   number: 71;
+  category: "Lanthanide";
 }
 
 export interface Hafnium extends Element {
   symbol: "Hf";
   number: 72;
+  category: "Transition Metal";
 }
 
 export interface Tantalum extends Element {
   symbol: "Ta";
   number: 73;
+  category: "Transition Metal";
 }
 
 export interface Tungsten extends Element {
   symbol: "W";
   number: 74;
+  category: "Transition Metal";
 }
 
 export interface Rhenium extends Element {
   symbol: "Re";
   number: 75;
+  category: "Transition Metal";
 }
 
 export interface Osmium extends Element {
   symbol: "Os";
   number: 76;
+  category: "Transition Metal";
 }
 
 export interface Iridium extends Element {
   symbol: "Ir";
   number: 77;
+  category: "Transition Metal";
 }
 
 export interface Platinum extends Element {
   symbol: "Pt";
   number: 78;
+  category: "Transition Metal";
 }
 
 export interface Gold extends Element {
   symbol: "Au";
   number: 79;
+  category: "Transition Metal";
 }
 
 export interface Mercury extends Element {
   symbol: "Hg";
   number: 80;
+  category: "Transition Metal";
 }
 
 export interface Thallium extends Element {
   symbol: "Tl";
   number: 81;
+  category: "Post-transition Metal";
 }
 
 export interface Lead extends Element {
   symbol: "Pb";
   number: 82;
+  category: "Post-transition Metal";
 }
 
 export interface Bismuth extends Element {
   symbol: "Bi";
   number: 83;
+  category: "Post-transition Metal";
 }
 
 export interface Polonium extends Element {
   symbol: "Po";
   number: 84;
+  category: "Metalloid";
 }
 
 export interface Astatine extends Element {
   symbol: "At";
   number: 85;
+  category: "Halogen";
 }
 
 export interface Radon extends Element {
   symbol: "Rn";
   number: 86;
+  category: "Noble Gas";
 }
 
 export interface Francium extends Element {
   symbol: "Fr";
   number: 87;
+  category: "Alkali Metal";
 }
 
 export interface Radium extends Element {
   symbol: "Ra";
   number: 88;
+  category: "Alkaline Earth Metal";
 }
 
 export interface Actinium extends Element {
   symbol: "Ac";
   number: 89;
+  category: "Actinide";
 }
 
 export interface Thorium extends Element {
   symbol: "Th";
   number: 90;
+  category: "Actinide";
 }
 
 export interface Protactinium extends Element {
   symbol: "Pa";
   number: 91;
+  category: "Actinide";
 }
 
 export interface Uranium extends Element {
   symbol: "U";
   number: 92;
+  category: "Actinide";
 }
 
 export interface Neptunium extends Element {
   symbol: "Np";
   number: 93;
+  category: "Actinide";
 }
 
 export interface Plutonium extends Element {
   symbol: "Pu";
   number: 94;
+  category: "Actinide";
 }
 
 export interface Americium extends Element {
   symbol: "Am";
   number: 95;
+  category: "Actinide";
 }
 
 export interface Curium extends Element {
   symbol: "Cm";
   number: 96;
+  category: "Actinide";
 }
 
 export interface Berkelium extends Element {
   symbol: "Bk";
   number: 97;
+  category: "Actinide";
 }
 
 export interface Californium extends Element {
   symbol: "Cf";
   number: 98;
+  category: "Actinide";
 }
 
 export interface Einsteinium extends Element {
   symbol: "Es";
   number: 99;
+  category: "Actinide";
 }
 
 export interface Fermium extends Element {
   symbol: "Fm";
   number: 100;
+  category: "Actinide";
 }
 
 export interface Mendelevium extends Element {
   symbol: "Md";
   number: 101;
+  category: "Actinide";
 }
 
 export interface Nobelium extends Element {
   symbol: "No";
   number: 102;
+  category: "Actinide";
 }
 
 export interface Lawrencium extends Element {
   symbol: "Lr";
   number: 103;
+  category: "Actinide";
 }
 
 export interface Rutherfordium extends Element {
   symbol: "Rf";
   number: 104;
+  category: "Transition Metal";
 }
 
 export interface Dubnium extends Element {
   symbol: "Db";
   number: 105;
+  category: "Transition Metal";
 }
 
 export interface Seaborgium extends Element {
   symbol: "Sg";
   number: 106;
+  category: "Transition Metal";
 }
 
 export interface Bohrium extends Element {
   symbol: "Bh";
   number: 107;
+  category: "Transition Metal";
 }
 
 export interface Hassium extends Element {
   symbol: "Hs";
   number: 108;
+  category: "Transition Metal";
 }
 
 export interface Meitnerium extends Element {
   symbol: "Mt";
   number: 109;
+  category: "Unknown";
 }
 
 export interface Darmstadtium extends Element {
   symbol: "Ds";
   number: 110;
+  category: "Unknown";
 }
 
 export interface Roentgenium extends Element {
   symbol: "Rg";
   number: 111;
+  category: "Unknown";
 }
 
 export interface Copernicium extends Element {
   symbol: "Cn";
   number: 112;
+  category: "Transition Metal";
 }
 
 export interface Nihonium extends Element {
   symbol: "Nh";
   number: 113;
+  category: "Unknown";
 }
 
 export interface Flerovium extends Element {
   symbol: "Fl";
   number: 114;
+  category: "Unknown";
 }
 
 export interface Moscovium extends Element {
   symbol: "Mc";
   number: 115;
+  category: "Unknown";
 }
 
 export interface Livermorium extends Element {
   symbol: "Lv";
   number: 116;
+  category: "Unknown";
 }
 
 export interface Tennessine extends Element {
   symbol: "Ts";
   number: 117;
+  category: "Unknown";
 }
 
 export interface Oganesson extends Element {
   symbol: "Og";
   number: 118;
+  category: "Unknown";
 }
