78849be73760c776f147da40282a2ad99b9a269b
ampcpmgp <ampcpmgp@gmail.com>
2024-04-09 22:22:49 +0900

    generate workspace

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -1,25 +1,31 @@
 import type { PlopTypes } from "@turbo/gen";
 
 export default function generator(plop: PlopTypes.NodePlopAPI): void {
-  plop.setGenerator("generate package", {
+  plop.setGenerator("generate workspace", {
     description:
-      "Create a new package in the monorepo with a README.md and package.json",
+      "Create a new workspace in the monorepo with a README.md and package.json",
     prompts: [
+      {
+        type: "list",
+        name: "type",
+        message: "What type of workspace is this?",
+        choices: ["apps", "packages"],
+      },
       {
         type: "input",
         name: "name",
-        message: "What is the name of the package?",
+        message: "What is the name of the workspace?",
       },
     ],
     actions: [
       {
         type: "add",
-        path: "{{ turbo.paths.root }}/packages/{{ name }}/package.json",
+        path: "{{ turbo.paths.root }}/{{ type }}/{{ name }}/package.json",
         templateFile: "templates/package.hbs",
       },
       {
         type: "add",
-        path: "{{ turbo.paths.root }}/packages/{{ name }}/README.md",
+        path: "{{ turbo.paths.root }}/{{ type }}/{{ name }}/README.md",
         templateFile: "templates/readme.hbs",
       },
     ],
