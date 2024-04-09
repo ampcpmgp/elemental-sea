a2d6a14f011bfbb327243706b21f34faedeba0e1
ampcpmgp <ampcpmgp@gmail.com>
2024-04-10 00:19:03 +0900

    close #10 biome settings

--- packages/element/src/constants/element-symbols.ts
+++ packages/element/src/constants/element-symbols.ts
@@ -1,35 +1,141 @@
-// prettier-ignore
 export const elementSymbols = [
-  // first
-  "H","He",
+	// first
+	"H",
+	"He",
 
-  // second
-  "Li","Be","B","C","N","O","F","Ne",
+	// second
+	"Li",
+	"Be",
+	"B",
+	"C",
+	"N",
+	"O",
+	"F",
+	"Ne",
 
-  // third
-  "Na","Mg","Al","Si","P","S","Cl","Ar",
+	// third
+	"Na",
+	"Mg",
+	"Al",
+	"Si",
+	"P",
+	"S",
+	"Cl",
+	"Ar",
 
-  // fourth
-  "K","Ca","Sc","Ti","V","Cr","Mn","Fe","Co","Ni","Cu","Zn","Ga","Ge","As","Se","Br","Kr",
+	// fourth
+	"K",
+	"Ca",
+	"Sc",
+	"Ti",
+	"V",
+	"Cr",
+	"Mn",
+	"Fe",
+	"Co",
+	"Ni",
+	"Cu",
+	"Zn",
+	"Ga",
+	"Ge",
+	"As",
+	"Se",
+	"Br",
+	"Kr",
 
-  // fifth
-  "Rb","Sr","Y","Zr","Nb","Mo","Tc","Ru","Rh","Pd","Ag","Cd","In","Sn","Sb","Te","I","Xe",
+	// fifth
+	"Rb",
+	"Sr",
+	"Y",
+	"Zr",
+	"Nb",
+	"Mo",
+	"Tc",
+	"Ru",
+	"Rh",
+	"Pd",
+	"Ag",
+	"Cd",
+	"In",
+	"Sn",
+	"Sb",
+	"Te",
+	"I",
+	"Xe",
 
-  // sixth
-  "Cs","Ba",
+	// sixth
+	"Cs",
+	"Ba",
 
-    // Lanthanides
-    "La","Ce","Pr","Nd","Pm","Sm","Eu","Gd","Tb","Dy","Ho","Er","Tm","Yb","Lu",
+	// Lanthanides
+	"La",
+	"Ce",
+	"Pr",
+	"Nd",
+	"Pm",
+	"Sm",
+	"Eu",
+	"Gd",
+	"Tb",
+	"Dy",
+	"Ho",
+	"Er",
+	"Tm",
+	"Yb",
+	"Lu",
 
-    // rest of sixth
-    "Hf","Ta","W","Re","Os","Ir","Pt","Au","Hg","Tl","Pb","Bi","Po","At","Rn",
-  
-  // seventh
-  "Fr","Ra",
+	// rest of sixth
+	"Hf",
+	"Ta",
+	"W",
+	"Re",
+	"Os",
+	"Ir",
+	"Pt",
+	"Au",
+	"Hg",
+	"Tl",
+	"Pb",
+	"Bi",
+	"Po",
+	"At",
+	"Rn",
 
-    // Actinides
-    "Ac","Th","Pa","U","Np","Pu","Am","Cm","Bk","Cf","Es","Fm","Md","No","Lr",
+	// seventh
+	"Fr",
+	"Ra",
 
-    // rest of seventh
-    "Rf","Db","Sg","Bh","Hs","Mt","Ds","Rg","Cn","Nh","Fl","Mc","Lv","Ts","Og"
+	// Actinides
+	"Ac",
+	"Th",
+	"Pa",
+	"U",
+	"Np",
+	"Pu",
+	"Am",
+	"Cm",
+	"Bk",
+	"Cf",
+	"Es",
+	"Fm",
+	"Md",
+	"No",
+	"Lr",
+
+	// rest of seventh
+	"Rf",
+	"Db",
+	"Sg",
+	"Bh",
+	"Hs",
+	"Mt",
+	"Ds",
+	"Rg",
+	"Cn",
+	"Nh",
+	"Fl",
+	"Mc",
+	"Lv",
+	"Ts",
+	"Og",
 ] as const;
--- packages/element/src/types/element.ts
+++ packages/element/src/types/element.ts
@@ -1,26 +1,26 @@
-import { elementSymbols } from "../constants/element-symbols";
+import type { elementSymbols } from "../constants/element-symbols";
 
 export interface Element {
-  symbol: ElementSymbol;
-  number: number;
-  category: ElementCategory;
+	symbol: ElementSymbol;
+	number: number;
+	category: ElementCategory;
 }
 
 // array to union
 export type ElementSymbol = (typeof elementSymbols)[number];
 
 export type ElementCategory =
-  | "Nonmetal"
-  | "Noble Gas"
-  | "Alkali Metal"
-  | "Alkaline Earth Metal"
-  | "Metalloid"
-  | "Halogen"
-  | "Post-transition Metal"
-  | "Transition Metal"
-  | "Lanthanide"
-  | "Actinide"
-  | "Unknown";
+	| "Nonmetal"
+	| "Noble Gas"
+	| "Alkali Metal"
+	| "Alkaline Earth Metal"
+	| "Metalloid"
+	| "Halogen"
+	| "Post-transition Metal"
+	| "Transition Metal"
+	| "Lanthanide"
+	| "Actinide"
+	| "Unknown";
 
 export type ElementPhase = "solid" | "liquid" | "gas";
 
@@ -31,9 +31,9 @@ export type ElementSource = "natural" | "synthetic";
 export type ElementAppearance = "silvery" | "dull" | "metallic" | "colorless";
 
 export type ElementAtomicStructure =
-  | "body-centered cubic"
-  | "face-centered cubic"
-  | "hexagonal close-packed";
+	| "body-centered cubic"
+	| "face-centered cubic"
+	| "hexagonal close-packed";
 
 export type ElementIonizationEnergy = "first" | "second" | "third";
 
@@ -46,41 +46,41 @@ export type ElementType = "element" | "isotope";
 export type ElementStandardState = "solid" | "liquid" | "gas";
 
 export type ElementBondingType =
-  | "metallic"
-  | "covalent network"
-  | "atomic"
-  | "metallic network"
-  | "ionic";
+	| "metallic"
+	| "covalent network"
+	| "atomic"
+	| "metallic network"
+	| "ionic";
 
 export type ElementMagneticType =
-  | "paramagnetic"
-  | "diamagnetic"
-  | "ferromagnetic"
-  | "antiferromagnetic"
-  | "superconductor";
+	| "paramagnetic"
+	| "diamagnetic"
+	| "ferromagnetic"
+	| "antiferromagnetic"
+	| "superconductor";
 
 export type ElementRadioactiveType =
-  | "primordial"
-  | "decay product"
-  | "from decay";
+	| "primordial"
+	| "decay product"
+	| "from decay";
 
 export type ElementOccurrence = "primordial" | "from decay";
 
 export type ElementGroupBlock = "s-block" | "p-block" | "d-block" | "f-block";
 
 export type ElementCrystalStructure =
-  | "simple cubic"
-  | "body-centered cubic"
-  | "face-centered cubic"
-  | "hexagonal";
+	| "simple cubic"
+	| "body-centered cubic"
+	| "face-centered cubic"
+	| "hexagonal";
 
 export type ElementHistory = "ancient" | "unknown" | "predicted" | "discovered";
 
 export type ElementDiscovery =
-  | "ancient"
-  | "unknown"
-  | "predicted"
-  | "discovered";
+	| "ancient"
+	| "unknown"
+	| "predicted"
+	| "discovered";
 
 export type ElementYear = number;
 
