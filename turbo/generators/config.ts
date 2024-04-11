9c8e39f146e84c342645f26997f535cd8a058c2b
ampcpmgp <ampcpmgp@gmail.com>
2024-04-11 19:06:40 +0900

    close #13, #19

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -1,43 +1,53 @@
 import type { PlopTypes } from "@turbo/gen";
 
 export default function generator(plop: PlopTypes.NodePlopAPI): void {
-  plop.setGenerator("generate workspace", {
-    description:
-      "Create a new workspace in the monorepo with a README.md and package.json",
-    prompts: [
-      {
-        type: "list",
-        name: "type",
-        message: "What type of workspace is this?",
-        choices: ["apps", "packages"],
-      },
-      {
-        type: "input",
-        name: "name",
-        message: "What is the name of the workspace?",
-      },
-    ],
-    actions: [
-      {
-        type: "add",
-        path: "{{ turbo.paths.root }}/{{ type }}/{{ name }}/package.json",
-        templateFile: "templates/package.hbs",
-      },
-      {
-        type: "add",
-        path: "{{ turbo.paths.root }}/{{ type }}/{{ name }}/README.md",
-        templateFile: "templates/readme.hbs",
-      },
-      {
-        type: "add",
-        path: "{{ turbo.paths.root }}/{{ type }}/{{ name }}/tsconfig.json",
-        templateFile: "templates/tsconfig.hbs",
-      },
-      {
-        type: "add",
-        path: "{{ turbo.paths.root }}/{{ type }}/{{ name }}/src/index.ts",
-        templateFile: "templates/index.hbs",
-      },
-    ],
-  });
+	plop.setGenerator("generate workspace", {
+		description:
+			"Create a new workspace in the monorepo with a package.json, tsconfig.json, and more.",
+		prompts: [
+			{
+				type: "list",
+				name: "type",
+				message: "What type of workspace is this?",
+				choices: ["apps", "packages"],
+			},
+			{
+				type: "input",
+				name: "name",
+				message: "What is the name of the workspace?",
+			},
+		],
+		actions: [
+			{
+				type: "add",
+				path: "{{ turbo.paths.root }}/{{ type }}/{{ name }}/package.json",
+				templateFile: "templates/package.hbs",
+			},
+			{
+				type: "add",
+				path: "{{ turbo.paths.root }}/{{ type }}/{{ name }}/README.md",
+				templateFile: "templates/readme.hbs",
+			},
+			{
+				type: "add",
+				path: "{{ turbo.paths.root }}/{{ type }}/{{ name }}/tsconfig.json",
+				templateFile: "templates/tsconfig.hbs",
+			},
+			{
+				type: "add",
+				path: "{{ turbo.paths.root }}/{{ type }}/{{ name }}/vitest.config.ts",
+				templateFile: "templates/vitest-config.hbs",
+			},
+			{
+				type: "add",
+				path: "{{ turbo.paths.root }}/{{ type }}/{{ name }}/src/index.ts",
+				templateFile: "templates/src/index.hbs",
+			},
+			{
+				type: "add",
+				path: "{{ turbo.paths.root }}/{{ type }}/{{ name }}/src/example.ts",
+				templateFile: "templates/src/example.hbs",
+			},
+		],
+	});
 }
---
+++ turbo/generators/templates/vitest-config.hbs
@@ -0,0 +1,6 @@
+import { defineConfig } from "vitest/config";
+import { baseTestConfig } from "vitest-config";
+
+export default defineConfig({
+	test: baseTestConfig,
+});
