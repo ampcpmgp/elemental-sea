d3e4d1b263837c1f8a73c78ae93e1b0e39ca11c9
ampcpmgp <ampcpmgp@gmail.com>
2024-04-12 08:31:53 +0900

    use element in app

--- apps/element-quiz/src/routes/+page.svelte
+++ apps/element-quiz/src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script type="ts">
+	import { elementSymbols } from 'element';
+</script>
+
+<ul>
+	{#each elementSymbols as symbol}
+		<li>{symbol}</li>
+	{/each}
+</ul>
