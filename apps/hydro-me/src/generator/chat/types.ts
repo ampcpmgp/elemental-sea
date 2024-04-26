ac5321ba2157858ce4c6468e675d676c37a97e32
ampcpmgp <ampcpmgp@gmail.com>
2024-04-27 08:35:45 +0900

    const model

--- apps/hydro-me/src/generator/chat/types.ts
+++ apps/hydro-me/src/generator/chat/types.ts
@@ -1,17 +1,20 @@
-import type { AnthropicInput, ChatAnthropic } from "@langchain/anthropic";
+import type { ChatAnthropic } from "@langchain/anthropic";
 import type { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import type { ChatOpenAI } from "@langchain/openai";
+import type {
+	anthoropicModelNames,
+	googleModelNames,
+	openaiModelNames,
+} from "./const";
 
 // https://docs.anthropic.com/claude/docs/models-overview
-export type AnthoropicModelName =
-	| "claude-3-haiku-20240307"
-	| "claude-3-sonnet-20240229";
+export type AnthoropicModelName = (typeof anthoropicModelNames)[number];
 
 // https://platform.openai.com/docs/models/gpt-4-turbo-and-gpt-4
-export type OpenAIModelName = "gpt-4-turbo-2024-04-09" | "gpt-3.5-turbo-0125";
+export type OpenAIModelName = (typeof openaiModelNames)[number];
 
 // https://ai.google.dev/gemini-api/docs/models/gemini?hl=ja#model-versions
-export type GoogleModelName = "gemini-1.0-pro";
+export type GoogleModelName = (typeof googleModelNames)[number];
 
 export type AnthoropicModelOption = ConstructorParameters<
 	typeof ChatAnthropic
