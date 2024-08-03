fcf265ff30ee94f404a3c47a1dfc19d26c95ea6e
ampcpmgp <ampcpmgp@gmail.com>
2024-08-03 09:10:01 +0900

    use gpt-4o-2024-05-13

--- apps/hydro-me/src/summary.ts
+++ apps/hydro-me/src/summary.ts
@@ -21,12 +21,12 @@ async function summary() {
 
 	prompt = Prompt.summary(article);
 
-	summary = await Generator.chat(prompt, "gpt-4-turbo-2024-04-09", {
+	summary = await Generator.chat(prompt, "gpt-4o-2024-05-13", {
 		temperature: 0,
 	});
 
 	prompt = Prompt.refine(summary, 360);
-	summary = await Generator.chat(prompt, "gpt-4-turbo-2024-04-09", {
+	summary = await Generator.chat(prompt, "gpt-4o-2024-05-13", {
 		temperature: 0,
 	});
 
